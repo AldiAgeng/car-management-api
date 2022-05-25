0650c19ad5e1e6130eb8cc954252f9b45c3b6611
AldiAgeng <aldiageng48@gmail.com>
2022-05-25 10:10:25 +0700

    add CORS policy

--- app/index.js
+++ app/index.js
@@ -6,6 +6,15 @@ const app = express();
 
 require("dotenv").config();
 
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 /** Install request logger */
 app.use(morgan("dev"));
 
